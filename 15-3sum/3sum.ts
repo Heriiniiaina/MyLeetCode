c75c4875f56c271faf645c9a95856e4e57bd0ef8
Heriniaina Ramamonjitiana <111340769+Heriiniiaina@users.noreply.github.com>
2024-12-13 21:11:34 +0300

    Time: 36 ms (64.40%) | Memory: 66.7 MB (56.43%) - LeetSync

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -1,9 +1,9 @@
 function threeSum(nums: number[]): number[][] {
     const triplets: number[][] = [];
-    nums.sort((a, b) => a - b); // Step 1: Sort the array
+    nums.sort((a, b) => a - b); 
 
     for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // Skip duplicates for the first element
+        if (i > 0 && nums[i] === nums[i - 1]) continue;  
 
         let left = i + 1;
         let right = nums.length - 1;
@@ -14,16 +14,16 @@ function threeSum(nums: number[]): number[][] {
             if (sum === 0) {
                 triplets.push([nums[i], nums[left], nums[right]]);
 
-                // Skip duplicates for the second and third elements
+                
                 while (left < right && nums[left] === nums[left + 1]) left++;
                 while (left < right && nums[right] === nums[right - 1]) right--;
 
                 left++;
                 right--;
             } else if (sum < 0) {
-                left++; // Increase the sum by moving the left pointer to the right
+                left++;
             } else {
-                right--; // Decrease the sum by moving the right pointer to the left
+                right--;
             }
         }
     }
